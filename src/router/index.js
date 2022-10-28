446925c908bab136cb974fee2910b8a9211428bf
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-10-28 09:11:12 +0400

    wip: add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import landingPage from "../components/landingPage.vue";
+import personalInformation from "../components/personalInformation.vue";
+import covidQuestions from "../components/covidQuestions.vue";
+import vaccinationPage from "../components/vaccinationPage.vue";
+import advisePage from "../components/advisePage.vue";
+import thankYou from "../components/thankYou.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "landingPage",
+      component: landingPage,
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/covid?page=1",
+      name: "personalInformation",
+      component: personalInformation,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      // component: () => import("../components/personalInformation.vue"),
+    },
+    {
+      path: "/covid?page=2",
+      name: "covidQuestions",
+      component: covidQuestions,
+    },
+    {
+      path: "/covid?page=3",
+      name: "vaccinationPage",
+      component: vaccinationPage,
+    },
+    {
+      path: "/covid?page=4",
+      name: "advisePage",
+      component: advisePage,
+    },
+    {
+      path: "/thanks",
+      name: "thanks",
+      component: thankYou,
     },
   ],
 });
