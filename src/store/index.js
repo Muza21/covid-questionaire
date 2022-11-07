c4f3a0d12cf6187a63211daeff114a47857ef033
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-11-07 18:05:54 +0400

    fix: state file structure

--- src/store/index.js
+++ src/store/index.js
@@ -2,31 +2,25 @@ import { createStore } from "vuex";
 
 import rootActions from "@/store/actions.js";
 import rootMutations from "@/store/mutations.js";
+import advisePage from "@/store/modules/advise-page/index.js";
+import personalInformation from "@/store/modules/personal-information/index.js";
+import vaccinationPage from "@/store/modules/vaccination-page/index.js";
+import covidQuestions from "@/store/modules/covid-questions/index.js";
 
 const store = createStore({
-  state() {
-    return {
-      name: "",
-      lastname: "",
-      email: "",
-
-      hadCovid: "",
-      testDone: "",
-      testDate: "",
-      covidAntigen: "",
-      covidDate: "",
-
-      hadVaccine: "",
-      stageLevel: "",
-      planAhead: "",
-
-      mettingNumber: "",
-      officeWork: "",
-      meetingOpinion: "",
-      adviseOpinion: "",
-    };
+  modules: {
+    personal: personalInformation,
+    covid: covidQuestions,
+    vaccine: vaccinationPage,
+    advise: advisePage,
   },
+
   actions: rootActions,
   mutation: rootMutations,
+  getters: {
+    getMeetingOpinion(state) {
+      return state.officeWork;
+    },
+  },
 });
 export default store;
