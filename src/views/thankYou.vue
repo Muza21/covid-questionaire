c0c080fe77015b3f5b8ba4cbf16c75f9cf7fca72
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-11-15 20:05:54 +0400

    fix: thank you page animation

--- src/views/thankYou.vue
+++ src/views/thankYou.vue
@@ -37,7 +37,7 @@
           y: -25,
         }"
         :enter="{ opacity: 1, x: -220, y: -25 }"
-        class="absolute transition-all duration-300 ease-in-out left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+        class="absolute transition-all delay-150 duration-150 ease-in-out left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
       >
         <img src="/src/assets/thanks.png" alt="thanks" />
       </div>
